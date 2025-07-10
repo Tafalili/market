5adff5452de7f4777668bfc65f4f8facee98abf8
Tafalili <ttofe90@gmail.com>
2025-07-10 13:14:14 +0300

    nav_store is DONE

--- lib/views/favorate/ui.dart
+++ lib/views/favorate/ui.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:market/views/home/home_tools/slider.dart';
+import '../../core/components/list_of_products.dart';
+import '../../core/components/search_fild.dart';
 
-class Nav_Favorate extends StatelessWidget {
+
+class Nav_Favorate extends StatefulWidget {
+  const Nav_Favorate({super.key});
+
+  @override
+  State<Nav_Favorate> createState() => _Nav_FavorateState();
+}
+
+class _Nav_FavorateState extends State<Nav_Favorate> {
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child:Center(child: Text("nav Favorate"),)),
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // Align text properly for RTL
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.only(right: 16, top: 8),
+                  // Adjusted for RTL
+                  child: Center(
+                    child: Text(
+                      "المنتجات المفضله",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                List_of_products()
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+
+
