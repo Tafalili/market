e5856bd34bb6735ae5736b3908855bfbd7df234b
Tafalili <ttofe90@gmail.com>
2025-07-11 21:39:17 +0300

    the card detils is done

--- lib/views/prouduct_detiles/detiles_of_product.dart
+++ lib/views/prouduct_detiles/detiles_of_product.dart
@@ -8,26 +8,31 @@ import 'package:market/views/prouduct_detiles/tools/rating_bar.dart';
 import '../../core/components/cach_image.dart';
 
 class Product_Detiles extends StatelessWidget {
-TextEditingController comment_controller = TextEditingController();
+  TextEditingController comment_controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Directionality(textDirection:TextDirection.rtl ,
+    return Directionality(
+        textDirection: TextDirection.rtl,
         child: Scaffold(
-          appBar: buildAppBar(  context, "معلومات المنتج"),
+          appBar: buildAppBar(context, "معلومات المنتج"),
           body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  cach_image(url: 'https://cdn.prod.website-files.com/5ee0a01b09389eebf4c09b45/65e607e3204a6d1354fc81d9_Rectangle%20995.webp',),
-                  SizedBox(height: 20,),
+                  cach_image(
+                    url:
+                        'https://cdn.prod.website-files.com/5ee0a01b09389eebf4c09b45/65e607e3204a6d1354fc81d9_Rectangle%20995.webp',
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text('شموع اروما'),
                       Text('20 الف'),
-              
-                      
                     ],
                   ),
                   Row(
@@ -36,26 +41,48 @@ TextEditingController comment_controller = TextEditingController();
                       Row(
                         children: [
                           Text('0'),
-                          Icon(Icons.star, color: Colors.amber,),
+                          Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
                         ],
                       ),
-                      IconButton(onPressed: (){}, icon: Icon(Icons.favorite,color: AppColors.kGreyColor,))
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.favorite,
+                            color: AppColors.kGreyColor,
+                          ))
                     ],
                   ),
-                 Center(
-                   child:
-                   rating(),
-                 ),
-                  SizedBox(height: 20,),
-                  CustomTFF(control_password:comment_controller , lable: 'ادخل تعليقك',suffexicon: Icon(Icons.send,)),
-                  SizedBox(height: 20,),
+                  Center(
+                    child: rating(),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CustomTFF(
+                      control_password: comment_controller,
+                      lable: 'ادخل تعليقك',
+                      suffexicon: Icon(
+                        Icons.send,
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Padding(
-                    padding: const EdgeInsets.only(right: 210),
-                    child: Text('Comments',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                  )
-              
+                    padding: const EdgeInsets.only(left: 230),
+                    child: Text('التعليقات',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
+                  ListView.separated(
+                    shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) => comment_list(),
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: 10)
                 ],
-                
               ),
             ),
           ),
@@ -63,3 +90,35 @@ TextEditingController comment_controller = TextEditingController();
   }
 }
 
+class comment_list extends StatelessWidget {
+  const comment_list({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 230),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'اسم المستخدم',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 35),
+            child: Text('التعليق....'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 72),
+            child: Text(
+              'رد',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
