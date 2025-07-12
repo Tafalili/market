331bdb60ac00cf4e20cde8485d6c2f2d57b156ba
Tafalili <ttofe90@gmail.com>
2025-07-12 11:41:24 +0300

    pairing supabase and dotnev to project:)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:market/home_page.dart';
 import 'package:market/views/auth/ui/login.dart';
 import 'package:market/views/auth/ui/WIDGETS/app_colors.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
